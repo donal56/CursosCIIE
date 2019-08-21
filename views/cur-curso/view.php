c2f845508d5fd9dd9c133944b9f29354cee350df
donal56 <donal_56@hotmail.com>
2019-08-21 18:04:58 -0500

    Merge

--- views/cur-curso/view.php
+++ views/cur-curso/view.php
@@ -1,54 +1,41 @@
 <?php
 
-use yii\helpers\Html;
-use yii\widgets\DetailView;
+    use yii\helpers\Html;
+    use yii\widgets\DetailView;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\CurCurso */
+    /* @var $this yii\web\View */
+    /* @var $model app\models\CurCurso */
 
-$this->title = $model->cur_id;
-$this->params['breadcrumbs'][] = ['label' => 'Cur Cursos', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+    $this->title = $model -> cur_nombre;
 ?>
-<div class="cur-curso-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="cur-curso-view main-body">
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->cur_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->cur_id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <br>
+    <div class= 'container'>
+        <h1 class= 'display-1' style= 'display: inline' ><?= $model->cur_nombre ?></h1>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'cur_id',
-            'cur_nombre',
-            'cur_dirigido',
-            'cur_cupo',
-            'cur_fechainicio',
-            'cur_fechafinal',
-            'cur_presentacion:ntext',
-            'cur_objetivo:ntext',
-            'cur_requisitos',
-            'cur_requerimientos',
-            'cur_duracion',
-            'cur_horario:ntext',
-            'cur_costo',
-            'cur_formaPago:ntext',
-            'cur_obtendra',
-            'cur_fkins_id',
-            'cur_temario',
-            'cur_procedimiento:ntext',
-            'cur_contacto:ntext',
-            'cur_observaciones:ntext',
-        ],
-    ]) ?>
+        <?= Html::a('', ['update', 'id' => $model->cur_id], ['class' => 'glyphicon glyphicon-pencil']) ?>
+        <?= Html::a('', ['delete', 'id' => $model->cur_id], [
+                'class' => 'glyphicon glyphicon-trash',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) 
+        ?>
+    </div>
 
+    <div class= 'wraper'>
+        <div class= 'blog_entries'>
+            <h2 align= 'center'> El Instituto Tecnológico de Villahermosa y el Centro de Incubación e Innovación Empresarial</h2>
+
+            <h4> Invitan a </h4>
+            <li></li>
+
+            
+
+
+        </div>
+    </div>
 </div>
