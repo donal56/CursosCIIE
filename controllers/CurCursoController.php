fbedb3120db8c3e522c10d60efe9bd69122ea20e
Sergio Daniel Jimenez Zcarias <hmaforsiv@gmail.com>
2019-08-26 17:44:16 -0500

    Update CurCursoController.php

--- controllers/CurCursoController.php
+++ controllers/CurCursoController.php
@@ -58,8 +58,9 @@ class CurCursoController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionView($id)
+    public function actionView()
     {
+        $id = CurCurso::getCurso()->cur_id;
         return $this->render('view', [
             'model' => $this->findModel($id),
         ]);
